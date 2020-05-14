877162daf1945332e7eda3e98fe86e4e1e8f7a25
Crowdbotics <team@crowdbotics.com>
2020-05-14 07:40:29 +0000

    Update models

--- home/api/v1/viewsets.py
+++ home/api/v1/viewsets.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from rest_framework import authentication
-from .serializers import CustomTextSerializer, HomePageSerializer, TestSerializer
+from .serializers import TestSerializer, HomePageSerializer, CustomTextSerializer
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.permissions import IsAdminUser
@@ -14,7 +14,7 @@ from home.api.v1.serializers import (
     HomePageSerializer,
     UserSerializer,
 )
-from home.models import CustomText, HomePage, Test
+from home.models import Test, HomePage, CustomText
 
 
 class SignupViewSet(ModelViewSet):
--- home/migrations/0005_test.py
+++ home/migrations/0005_test.py
@@ -6,15 +6,23 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0004_remove_customtext_hgfghfh'),
+        ("home", "0004_remove_customtext_hgfghfh"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Test',
+            name="Test",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('hjgjh', models.BigIntegerField()),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("hjgjh", models.BigIntegerField()),
             ],
         ),
     ]
