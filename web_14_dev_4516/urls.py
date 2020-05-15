d6b735ff5bd675165e236a4b12e2dfa1d269e56a
Crowdbotics <team@crowdbotics.com>
2020-05-15 11:29:08 +0000

    Update admin title

--- web_14_dev_4516/urls.py
+++ web_14_dev_4516/urls.py
@@ -33,9 +33,9 @@ urlpatterns = [
     path("home/", include("home.urls")),
 ]
 
-admin.site.site_header = "web 14"
-admin.site.site_title = "web 14 Admin Portal"
-admin.site.index_title = "web 14 Admin"
+admin.site.site_header = "web 144"
+admin.site.site_title = "web 144 Admin Portal"
+admin.site.index_title = "web 144 Admin"
 
 # swagger
 schema_view = get_schema_view(
